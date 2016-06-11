823dbb8a4a587eee6de7c654d3bfa444089e2f3c
Zhengguo GU <z.gu@uvt.nl>
2016-06-11 16:52:46 +0200

    corrected a mistake; The results are the same to the ones generated by mySTD funciton on Matlab

--- R/mySTD.R
+++ R/mySTD.R
@@ -3,7 +3,7 @@
 #' @param DATA A data matrix
 #' @return a standardized matrix
 #' @examples
-#' mySTD(matrix(1:12), nrow=3, byrow=T)
+#' mySTD(matrix(1:12), nrow=3, byrow=T))
 
 mySTD <- function(DATA) {
   nrow_data <- dim(DATA)[1]
@@ -11,7 +11,7 @@ mySTD <- function(DATA) {
   v <- matrix(1, nrow = nrow_data, ncol = nrow_data)  # note here: it's a square matrix
   DATAc <- DATA - v %*% DATA/nrow_data
   CP <- v %*% (DATAc ^ 2)
-  STDDATA <- sqrt(nrow_data) * DATAc / (CP ^ 0.5)
+  STDDATA <- sqrt(nrow_data-1) * DATAc / (CP ^ 0.5)
 
   return(STDDATA)
 }
