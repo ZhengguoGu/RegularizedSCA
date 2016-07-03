8c1019dce389cd969059795cb3b00dd8956af325
Zhengguo GU <z.gu@uvt.nl>
2016-07-03 15:39:45 +0200

    this function needs to be tested

---
+++ R/VarSelectComDistPre.R
@@ -0,0 +1,149 @@
+#Variable selection algorithm when the common/distinctive structure is known a priori. The common component can also be sparse, which is to be estimated by Lasso. The distinctive components are not sparse in the sense that the entire variables in a component (belonging to a certain block) are either all zeros or non-zeros.
+
+
+VarSelectComDistPre <- function(DATA, Jk, R, CommPosition, GroupStructure, LASSO, maxiter){
+
+  DistPosition <- setdiff(1:R, CommPosition)
+  I_Data <- dim(DATA)[1]
+  sumJk <- dim(DATA)[2]
+  eps <- 10^(-12)
+
+  #initialize P, Lossc
+  P <- matrix(rnorm(sumJk * R), nrow = sumJk, ncol = R)
+  P[GroupStructure == 0]<-0
+  Pt <- t(P)
+
+  PIndexforLasso <- Pt
+  PIndexforLasso[CommPosition, ] <- 1
+  PIndexforLasso[DistPosition, ] <- 0
+  PIndexforGLasso <- Pt #note that the distinctive structure is predefined.
+  PIndexforGLasso[CommPosition, ] <- 0
+  PIndexforGLasso[DistPosition, ] <- 1
+
+  absP <- abs(P)
+  pen1 <- LASSO*sum(abs(P[, CommPosition]))
+  sqP <- P^2
+  L <- 1
+  sumpk <- array()
+  sqrtpk <- array()
+  for (i in 1:length(Jk)){
+
+    U <- L + Jk[i] - 1
+    sqP4Dist <- sqP
+    sqP4Dist[ , CommPosition] <- 0
+    sqrtsumP <- sqrt(colSums(sqP4Dist[L:U, ]))/sqrt(Jk[i])
+    sqrtsumPDist <- sqrt(colSums(sqP4Dist[L:U, DistPosition]))/sqrt(Jk[i])
+    sqrtpk[i] <- matrix(1, Jk[i], 1) %*% sqrtsumP
+    L <- U + 1
+
+  }
+
+  residual <- sum(DATA^2)
+  Lossc <- residual + pen1
+
+  conv <- 0
+  iter <- 1
+  Lossvec <- array()
+
+  while (conv == 0){
+
+    #update Tmat, note that Tmax refers to T matrix
+    if (LASSO == 0){
+      SVD_DATA <- svd(DATA, R, R)  #note this is different from the matlab svds function. need to test it!!
+      Tmat <- SVD_DATA$u
+    }
+    else {
+      A <- Pt %*% t(DATA)
+      SVD_DATA <- svd(A, R, R)
+      Tmat <- SVD_DATA$v %*% t(SVD_DATA$u)
+    }
+
+    residual <- sum((DATA - Tmat %*% Pt)^2)
+    Lossu <- residual + pen1
+
+    #update P
+    if (LASSO == 0){
+      P <- t(DATA) %*% Tmat
+      P[GroupStructure == 0] <- 0  # this is to keep the zero structures
+      Pt <- t(P)
+    }
+    else{
+
+      for (r in 1:R){
+        if (r %in% CommPosition) {
+          for (j in 1:sumJk){
+            ols <- t(DATA[, j]) %*% Tmat[, r]
+            Lambda <- 0.5 * LASSO
+            if (ols < 0 & abs(ols) > Lambda) {
+              P[j, r] <- ols + Lambda
+            }
+            else if (ols > 0 & abs(ols) > Lambda) {
+              P[j, r] <- ols - Lambda
+            }
+            else {
+              P[j, r] <- 0
+            }
+          }
+        }
+        else {
+          for (j in 1:sumJk){
+            P[j, r] <- t(DATA[, j]) %*% Tmat[, r] #note that in the original matlab file this term is devided by sumD(=1)
+          }
+        }
+      }
+    }
+
+    P[GroupStructure == 0] <- 0
+    Pt <- t(P)
+
+    absP <- abs(P)
+    pen1 <- LASSO*sum(abs(P[, CommPosition]))
+    sqP <- P^2
+
+    L <- 1
+    sumpk <- array()
+    sqrtpk <- array()
+    for (i in 1:length(Jk)){
+
+      U <- L + Jk[i] - 1
+      sqP4Dist <- sqP
+      sqP4Dist[ , CommPosition] <- 0
+      sqrtsumP <- sqrt(colSums(sqP4Dist[L:U, ]))/sqrt(Jk[i])
+      sqrtsumPDist <- sqrt(colSums(sqP4Dist[L:U, DistPosition]))/sqrt(Jk[i])
+      sqrtpk[i] <- matrix(1, Jk[i], 1) %*% sqrtsumP
+      L <- U + 1
+    }
+    residual <- sum((DATA - Tmat %*% Pt)^2)
+    Lossu2 <- residual + pen1
+
+    #check convergence
+    if (abs(Lossc-Lossu)< 10^(-9)) {
+      Loss <- Lossu
+      residual <- residual
+      lassopen <- pen1
+      P[abs(P) <= 2 * eps] <- 0
+      conv <- 1
+    }
+    else if (iter == maxiter | LASSO == 0){
+      Loss <- Lossu
+      residual <- residual
+      lassopen <- pen1
+      P[abs(P) <= 2 * eps] <- 0
+      conv <- 1
+    }
+
+    Lossvec[iter] <- Lossu
+    iter <- iter + 1
+    Lossc <- Lossu2
+  }
+
+  return_varselect <- list()
+  return_varselect$Pmatrix <- P
+  return_varselect$Tmatrix <- Tmat
+  return_varselect$Loss <- Loss
+  return_varselect$Lossvec <- Lossvec
+  return_varselect$residual <- residual
+  return_varselect$lassopen <- lassopen
+  return(return_varselect)
+
+}
