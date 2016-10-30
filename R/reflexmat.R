fd9497bfd27c18dba4205d4a4c3e80daf1fc7798
Zhengguo PC <z.gu@uvt.nl>
2016-10-30 16:37:44 +0100

    a function for constructing matrix of reflections

---
+++ R/reflexmat.R
@@ -0,0 +1,24 @@
+###################################################
+#  a function to construct matrix of reflections
+#  author: Katrijn Van Duen
+###################################################
+
+reflexmat <- function(m){
+
+  mat <- rep(1, m)
+
+  for(i in 1:(m-1)){
+
+    B <- combn(1:m, i)
+
+      for(j in 1:dim(B)[2]){
+
+        v <- rep(1, m)
+        v[t(B[, j])] <- -1
+
+        mat <- rbind(mat, v)
+      }
+  }
+
+  return(mat)
+}
