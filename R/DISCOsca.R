16009f07d857aefacdc176d22ea610849e5fa83e
Zhengguo PC <z.gu@uvt.nl>
2016-11-12 12:20:58 +0100

    further improvement of the function

--- R/DISCOsca.R
+++ R/DISCOsca.R
@@ -14,6 +14,11 @@ DISCOsca <- function(DATA, R, Jk){
 
 #find all the combinations
 
+  TARGET_list <- list()
+  TROT_list <- list()
+  PROT_list <- list()
+  Posit_indicatorList <- list()
+
   for(i in max(num_block, R): (R*num_block)){
 
     combinations <- combn(matrix(1:(R*num_block), num_block, R), i)
@@ -23,83 +28,105 @@ DISCOsca <- function(DATA, R, Jk){
       posit_indicator <- matrix(0, num_block, R)
       posit_indicator[combinations[,j]] <- 1
 
-      if (min(rowSums(posit_indicator))==0 | min(colSums(posit_indicator))==0){
-        # in this case, either some blocks are entirely zero, or some components are entirely zero,
-        # which does not make sense.
-        break
+      if (min(rowSums(posit_indicator))!=0 & min(colSums(posit_indicator))!=0){
+        # if function is to remove cases where an entire column/block is 0
 
-        } else {
+        Posit_indicatorList <- c(Posit_indicatorList, list(posit_indicator))
+        W <- matrix(0, sum(Jk), R)
+        L <- 1
 
-          W <- matrix(0, sum(Jk), R)
-          L <- 1
+        for (k in 1:length(Jk)){
 
-          for (k in 1:length(Jk)){
+          U <- L + Jk[k] - 1
+          W_part <- W[L:U, ]
+          W_part[, (posit_indicator[k, ]==1)] <- 1
+          W[L:U, ] <- W_part
+          L <- U + 1
 
-            U <- L + Jk[k] - 1
-            W_part <- W[L:U, ]
-            W_part[, (posit_indicator[k, ]==1)] <- 1
-            W[L:U, ] <- W_part
-            L <- U + 1
+        }
 
-          }
+        Target <- 1 - W
+        TARGET_list <- c(TARGET_list,list(Target))
 
-          Target <- 1 - W
+        maxiter <- 600;
+        convergence <- 0.0000001;
+        nrstarts <- 5  #pre=5
 
-          maxiter <- 600;
-          convergence <- 0.0000001;
-          nrstarts <- 5
+        LOSS <- array()
+        BMAT <- list()
+        for (i in 1:nrstarts){
 
-          LOSS <- array()
-          BMAT <- list()
-          for (i in 1:nrstarts){
+          B <- pstr(Pmat,Target,W,maxiter,convergence)
+          loss <- pstrLoss(B$Bmatrix,Pmat,Target,W)
+          LOSS[i] <- loss
+          BMAT[[i]] <- B$Bmatrix
 
-            B <- pstr(Pmat,Target,W,maxiter,convergence)
-            loss <- pstrLoss(B$Bmatrix,Pmat,Target,W)
-            LOSS[i] <- loss
-            BMAT[[i]] <- B$Bmatrix
+        }
+        k <- which(LOSS == min(LOSS))
+        if (length(k)>1){  #ask Katrijn
+          k <- 1
+        }
+        B <- BMAT[[k]]
+        Trot <- Tmat %*% B
+        Prot <- Pmat %*% B
+        B <- diag(R)
 
-          }
-          k <- which(LOSS == min(LOSS))
-          B <- BMAT[[k]]
-          Trot <- Tmat %*% B
-          Prot <- Pmat %*% B
-          B <- diag(R)
-
-          if(sum(duplicated(posit_indicator,MARGIN=2)) > 0){
-            #if > 0, some of the components are the same in terms of
+        if(sum(duplicated(posit_indicator,MARGIN=2)) > 0){
+            #if > 0, some of the components are the same (in terms of the comm/dist components in the blocks)
 
             ind <- c(1:R)
+
             posit_indicator_dup <- posit_indicator
+
             while(length(ind)>1){
 
               ind_duplicate <- vector()
-              for(r in 1:length(ind)){
-                if(sum(posit_indicator_dup[,1] == posit_indicator_dup[, r])==num_block){
-                  ind_duplicate <- cbind(ind_duplicate, r)
+
+              for(j in 2:length(ind)){
+
+                if(sum(posit_indicator_dup[,1] == posit_indicator_dup[, j])==num_block){
+                  ind_duplicate <- cbind(ind_duplicate, j)
                 }
+
               }
+
+              if(length(ind_duplicate)==0){
+                # in this case, it means either the first column (indexed by ind) is unique, or is the only column left
+                ind <- ind[-1]
+                posit_indicator_dup <- as.matrix(posit_indicator_dup[, -1]) #as.matrix is needed for the case where only one column is left.
+              } else{
 
               ind_duplicate <- c(1, ind_duplicate) #now we have an index of duplicate columns that are the same as the first column
               ind_dupMATCHind <- ind[ind_duplicate] # here we know which columns in Pmat are duplicated
               dupli_rotate <- svd(Prot[, ind_dupMATCHind], length(ind_dupMATCHind),length(ind_dupMATCHind) )
-              B[ind_dupMATCHind, ind_dupMATCHind] <- dupli_rotate$v
+              B[ind_dupMATCHind, ind_dupMATCHind] <- dupli_rotate$v  #ask Katrijn
               ind <- ind[-ind_duplicate] # remove the duplicated and start searching duplicates in the next columns
-              posit_indicator_dup <- posit_indicator_dup[, -ind_duplicate]
+              posit_indicator_dup <- as.matrix(posit_indicator_dup[, -ind_duplicate]) #as.matrix is needed for the case where only one column is left.
+              }
 
             }
+
           }
 
-          Trot <- Trot %*% B
-          Prot <- Prot %*% B
+        Trot <- Trot %*% B
+        Prot <- Prot %*% B
 
 
+        TROT_list <- c(TROT_list, list(Trot))
+        PROT_list <- c(PROT_list, list(Prot))
 
-        }
-
-
+      }
 
     }
 
   }
-  zz <- combn(dd, 4)
+  results <- list()
+  results$Target_matrix <- TARGET_list
+  results$Postion_indicator <- Posit_indicatorList
+  results$Tmatrix <- Tmat
+  results$Pmatrix <- Pmat
+  results$Trot <- TROT_list
+  results$Prot <- PROT_list
+  return(results)
+
 }
