4ad7c1ef00451f9505a380a5a02bdc7139140926
Zhengguo Gu <z.gu@uvt.nl>
2017-06-16 12:56:44 +0200

    replace MezzichSolomon data with HerringData.

---
+++ R/HerringData.R
@@ -0,0 +1,19 @@
+#' Herring data 
+#' 
+#' This dataset contains data from ripening experiments of herring regarding the physical/chemical changes observed 
+#' in the herrings and quantitative descriptive sensory evaluation on the same herrings.
+#' @format The dataset contains the following list:
+#' \describe{
+#'  \item{Herring_ChemPhy}{A 7x10 matrix of observation x physical/chemical changes of herrings.}
+#'  \item{Herring_Sensory}{A 7x10 matrix of observation x sensary data of herrings}
+#' }
+#' 
+#' @note 
+#' This dataset is a small part of a large, publically avaiable dataset stored at http://www.models.life.ku.dk.
+#' 
+#' @source 
+#' This dataset is part of the herring dataset at http://www.models.life.ku.dk. Also see, 1) Rasmus Bro, Henrik Hauch Nielsen, Guðmundur Stefánsson, 
+#' Torstein Skåra, A Phenomenological Study of Ripening of Salted Herring. Assessing homogeneity of data from different countries and laboratories; J. Chemom., 16:81-88, 2002
+#' and 2) Nielsen HH, Bro R, Stefansson G, Skåra T, Salting and ripening of herring - collection and analysis of research results and industrial experience within the Nordic countries, 
+#' TemaNord 1999:578, ISBN 92-893-0371-9, Nordic Council of Ministers, 1999.
+"HerringData"
