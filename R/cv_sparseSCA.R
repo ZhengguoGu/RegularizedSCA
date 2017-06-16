62550fd033172900f6f57d3099612ff224e52766
Zhengguo Gu <z.gu@uvt.nl>
2017-06-16 20:27:55 +0200

    fixed a bug

--- R/cv_sparseSCA.R
+++ R/cv_sparseSCA.R
@@ -67,6 +67,7 @@ cv_sparseSCA <- function(DATA, Jk, R, MaxIter, NRSTARTS, LassoSequence, GLassoSe
       GLassomax <- results$Glasso
       Lassomax <- results$Lasso
 
+      
     if(missing(LassoSequence) & missing(GLassoSequence)){
       LassoSequence <- exp(seq(from = log(0.00000001), to = log(Lassomax), length.out = 20))
       GLassoSequence <- seq(from = 0.00000001, to = GLassomax, length.out = 10)  #note that Glasso is not on the log scale, because it is not helpful.
@@ -76,12 +77,13 @@ cv_sparseSCA <- function(DATA, Jk, R, MaxIter, NRSTARTS, LassoSequence, GLassoSe
         plotlog <- 1
     } else if(missing(GLassoSequence) & (length(LassoSequence) == 1)){
         GLassoSequence <- seq(from = 0.00000001, to = GLassomax, length.out = 50)
-    } else if (!missing(LassoSequence) & missing(GLassoSequence)){
+    } else if(missing(GLassoSequence) & (length(LassoSequence)>1)){
         GLassoSequence <- seq(from = 0.00000001, to = GLassomax, length.out = 10)
-    } else if (!missing(GLassoSequence) & missing(LassoSequence)){
+    } else if(missing(LassoSequence) & (length(GLassoSequence)>1)){
         LassoSequence <- exp(seq(from = log(0.00000001), to = log(Lassomax), length.out = 20))
         plotlog <- 1
     }
+    
     
   }
 
