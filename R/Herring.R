87aba6286a0f27f1f8946122291cfa2c709e2725
Zhengguo Gu <z.gu@uvt.nl>
2017-06-23 13:53:45 +0200

    update

--- R/Herring.R
+++ R/Herring.R
@@ -4,16 +4,16 @@
 #' in the herrings and quantitative descriptive sensory evaluation on the same herrings.
 #' @format The dataset contains the following list:
 #' \describe{
-#'  \item{ChemPhy}{A 7x10 matrix of observation x physical/chemical changes of herrings.}
-#'  \item{Sensory}{A 7x10 matrix of observation x sensary data of herrings}
+#'  \item{Herring_ChemPhy}{A 7x10 matrix of observation x physical/chemical changes of herrings.}
+#'  \item{Herring_Sensory}{A 7x10 matrix of observation x sensary data of herrings}
 #' }
 #' 
 #' @note 
 #' This dataset is a small part of a large, publically avaiable dataset stored at http://www.models.life.ku.dk.
 #' 
 #' @source 
-#' This dataset is part of the herring dataset at http://www.models.life.ku.dk. Also see, 1) Rasmus Bro, Henrik Hauch Nielsen, Guðmundur Stefánsson, 
-#' Torstein Skåra, A Phenomenological Study of Ripening of Salted Herring. Assessing homogeneity of data from different countries and laboratories; J. Chemom., 16:81-88, 2002
-#' and 2) Nielsen HH, Bro R, Stefansson G, Skåra T, Salting and ripening of herring - collection and analysis of research results and industrial experience within the Nordic countries, 
+#' This dataset is part of the herring dataset at http://www.models.life.ku.dk. Also see, 1) Rasmus Bro, Henrik Hauch Nielsen, Gu<c3><b0>mundur Stef<c3><a1>nsson, 
+#' Torstein Sk<c3><a5>ra, A Phenomenological Study of Ripening of Salted Herring. Assessing homogeneity of data from different countries and laboratories; J. Chemom., 16:81-88, 2002
+#' and 2) Nielsen HH, Bro R, Stefansson G, Sk<c3><a5>ra T, Salting and ripening of herring - collection and analysis of research results and industrial experience within the Nordic countries, 
 #' TemaNord 1999:578, ISBN 92-893-0371-9, Nordic Council of Ministers, 1999.
 "Herring"
