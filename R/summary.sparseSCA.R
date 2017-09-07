282c7d6b37f8931dc5a8baa3d923941f4bb5e977
Zhengguo Gu <z.gu@uvt.nl>
2017-09-07 18:02:49 +0200

    update

--- R/summary.sparseSCA.R
+++ R/summary.sparseSCA.R
@@ -1,10 +1,14 @@
 #'Display a summary of the results of Cross-validation of \code{sparseSCA()}.
 #'
 #'@param object Object of class inheriting from 'sparseSCA'.
-#'@param disp The default is \code{simple}; in this case, the recommended tuning parameter values will be displayed. 
-#'            If \code{full}, then information is displayed regarding 1) the recommended tuning parameter values, 2) the proper region 
-#'            for Lasso tuning parameter values, given a Group Lasso tuning parameter value, 3) # of variable selected,
-#'            4) Predicted residual sum of squares (PRESS), 5) standard errors for PRESS, 6) Lasso and Group Lasso tuning 
+#'@param disp The default is \code{simple}; in this case, the recommended tuning 
+#'            parameter values will be displayed. 
+#'            If \code{full}, then information is displayed regarding 1) the 
+#'            recommended tuning parameter values, 2) the proper region 
+#'            for Lasso tuning parameter values, given a Group Lasso tuning 
+#'            parameter value, 3) # of variable selected,
+#'            4) Predicted residual sum of squares (PRESS), 
+#'            5) standard errors for PRESS, 6) Lasso and Group Lasso tuning 
 #'            parameter values that have been evaluated.
 #'@examples
 #'\dontrun{
@@ -58,6 +62,5 @@ summary.sparseSCA <- function(obj_sSCA, disp){
   }else{
     stop("either simple or full")
   }
-  
   
 }
