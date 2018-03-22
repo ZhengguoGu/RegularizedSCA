43b3fdd8ae4cb75dff27dbd83b21dd7b5a020f2e
Zhengguo Desktop Home <z.gu@uvt.nl>
2018-03-22 21:56:22 +0100

    update

--- R/pre_process.R
+++ R/pre_process.R
@@ -34,14 +34,14 @@ pre_process <- function(DATA, weight) {
   nrow_data <- dim(DATA)[1]
   ncol_data <- dim(DATA)[2]
   
-  if(isTRUE(weight)){
-    DATA <- DATA/sqrt(ncol_data)
-  }
-  
   v <- matrix(1, nrow = nrow_data, ncol = nrow_data)  # note here: it's a square matrix
   DATAc <- DATA - v %*% DATA/nrow_data
   CP <- v %*% (DATAc ^ 2)
   STDDATA <- sqrt(nrow_data-1) * DATAc / (CP ^ 0.5)
+  
+  if(isTRUE(weight)){
+    DATA <- DATA/sqrt(ncol_data)
+  }
   
   return(STDDATA)
 }
